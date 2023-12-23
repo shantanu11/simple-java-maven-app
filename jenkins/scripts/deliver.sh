04dbde48bb8fc041936c794571e325239c2500a9
Shantanu <shan123pra@gmail.com>
2023-12-23 07:43:48 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,26 +11,22 @@ set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-# Extract the project name and version from the pom.xml
+# Extracting the project name and version from pom.xml
 echo 'Extracting the project name and version from pom.xml'
 set -x
-NAME=$(mvn help:evaluate -Dexpression=project.artifactId | grep "^[^\[]" | tr -d '[:space:]')
-VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "^[^\[]" | tr -d '[:space:]')
+NAME=$(mvn help:evaluate -Dexpression=project.artifactId | grep -E '^\[.*' | tr -d '[:space:]')
+VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -E '^\[.*' | tr -d '[:space:]')
 set +x
 
-# Echo the name and version
-echo "Project Name: $NAME"
-echo "Version: $VERSION"
-
-# Execute the Java application using the jar file
+# Running the Java application
 echo 'Running the Java application.'
 set -x
 
-# Constructing the jar file name based on the extracted project name and version
 JAR_NAME="${NAME}-${VERSION}.jar"
 JAR_PATH="target/${JAR_NAME}"
 
-# Check if the jar exists and is a file before attempting to run
+echo "Looking for jar at: $JAR_PATH"
+
 if [ -f "$JAR_PATH" ]; then
     java -jar $JAR_PATH
 else
