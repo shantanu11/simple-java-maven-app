17ebeecc52646c1f4450e317d60aa907d618eac7
Shantanu <shan123pra@gmail.com>
2023-12-23 07:46:37 +0530

    Update_3 deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,27 +6,25 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 
-# Install the project into the local Maven repository
+#!/usr/bin/env bash
+
+#The following commands install your Java application into the local Maven repository
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-# Extracting the project name and version from pom.xml
-echo 'Extracting the project name and version from pom.xml'
-set -x
-NAME=$(mvn help:evaluate -Dexpression=project.artifactId | grep -E '^\[.*' | tr -d '[:space:]')
-VERSION=$(mvn help:evaluate -Dexpression=project.version | grep -E '^\[.*' | tr -d '[:space:]')
-set +x
+# Extracting project name and version from the pom.xml
+NAME=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
-# Running the Java application
-echo 'Running the Java application.'
-set -x
-
+# Constructing jar file name and path
 JAR_NAME="${NAME}-${VERSION}.jar"
 JAR_PATH="target/${JAR_NAME}"
 
-echo "Looking for jar at: $JAR_PATH"
+# Printing the expected path for debugging
+echo "Expected jar path: $JAR_PATH"
 
+# Running the Java application if the jar exists
 if [ -f "$JAR_PATH" ]; then
     java -jar $JAR_PATH
 else
